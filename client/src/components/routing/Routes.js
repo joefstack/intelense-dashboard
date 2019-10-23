864e8234c8c6a78f5235bb8d778198ad4752ee52
joefstack <joefstack@gmail.com>
2019-10-22 21:36:34 -0400

    updated styling

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Register from '../auth/Register';
-import Login from '../auth/Login';
-import Alert from '../layout/Alert';
-import Dashboard from '../dashboard/Dashboard';
-import NotFound from '../layout/NotFound';
-import PrivateRoute from '../routing/PrivateRoute';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Register from "../auth/Register";
+import Login from "../auth/Login";
+import Alert from "../layout/Alert";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "../layout/NotFound";
+import PrivateRoute from "../routing/PrivateRoute";
 
 const Routes = () => {
   return (
-    <section className='container'>
+    <section className="container">
       <Alert />
       <Switch>
-        <Route exact path='/register' component={Register} />
-        <Route exact path='/login' component={Login} />
-        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <Route component={NotFound} />
       </Switch>
     </section>
