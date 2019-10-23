678da39d016746104de65a7b469f640043e64f78
joefstack <joefstack@gmail.com>
2019-10-23 15:51:33 -0400

    added backend packages for cookies and sessions

--- server.js
+++ server.js
@@ -2,15 +2,46 @@ const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
 
+const Session = require("express-session"),
+  bodyParser = require("body-parser"),
+  FileStore = require("session-file-store")(Session),
+  config = require("./config/default"),
+  flash = require("connect-flash");
+
 const app = express();
 
-// server config
+// connect db
 connectDB();
+
+// server middleware config
 app.use(express.json({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+app.use(express.static("public"));
+app.use(flash());
+app.use(require("cookie-parser")());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
+app.use(
+  Session({
+    store: new FileStore({
+      path: "./sessions"
+    }),
+    secret: config.sessionSecret,
+    maxAge: Date().now + 60 * 1000 * 30,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
